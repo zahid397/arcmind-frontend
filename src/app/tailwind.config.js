9a4c9b718f6179a6e9561fe26ac5d4b5e5910107
zahid397 <223230117+zahid397@users.noreply.github.com>
2026-01-17 23:43:18 +0600

    Create tailwind.config.js

---
+++ src/app/tailwind.config.js
@@ -0,0 +1,20 @@
+theme: {
+  extend: {
+    colors: {
+      neon: {
+        green: '#00ff00',
+        cyan: '#00ffff',
+        blue: '#0088ff',
+        pink: '#ff00ff',
+        yellow: '#ffff00',
+      },
+      compost: {
+        950: '#0b0a07',
+        900: '#14110b',
+        800: '#1e1a11',
+        700: '#2a2416',
+        600: '#3a311e',
+      },
+    },
+  },
+    }
