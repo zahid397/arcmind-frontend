1f07ae8743364353a6a23345791c46c6facd5db8
zahid397 <223230117+zahid397@users.noreply.github.com>
2026-01-17 22:11:21 +0600

    Create utils.ts

---
+++ src/app/lib/utils.ts
@@ -0,0 +1,18 @@
+// src/app/lib/utils.ts
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]) {
+return twMerge(clsx(inputs))
+}
+
+export function formatTime(date: Date): string {
+return new Intl.DateTimeFormat('en-US', {
+hour: '2-digit',
+minute: '2-digit'
+}).format(date)
+}
+
+export function generateMessageId(): string {
+return ${Date.now()}-${Math.random().toString(36).substr(2, 9)}
+}
