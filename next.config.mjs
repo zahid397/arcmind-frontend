b77059e5b727e1eb390a30d17461fba5c05e785d
zahid397 <223230117+zahid397@users.noreply.github.com>
2026-01-20 07:20:33 +0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    // হ্যাকাথনের জন্য: ছোটখাটো এরর ইগনোর করবে যাতে ডিপ্লয় না আটকায়
-    ignoreDuringBuilds: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
   },
-  typescript: {
-    // টাইপস্ক্রিপ্ট এরর হলেও বিল্ড হবে
-    ignoreBuildErrors: true,
-  },
-};
+}
 
-export default nextConfig;
+export default nextConfig
